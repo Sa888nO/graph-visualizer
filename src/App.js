f8707fc9ba0673520aa335231612306930a6cf6e
Sa888nO <shura.reno.97@bk.ru>
2022-10-25 09:04:51 +0300

    created graphDraw component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,35 @@
-function App() {
-	return <A>df</A>;
-}
+import GraphDraw from "@components/Graph/GraphDraw";
+import styled from "styled-components";
+import Graph from "vis-react";
+
+const GraphBlock = styled.div`
+	height: 90vh;
+	width: 60vw;
+	border: 3px solid black;
+	border-radius: 20px;
+`;
+const Content = styled.div`
+	padding: 20px;
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+`;
+
+const App = () => {
+	return (
+		<Content>
+			<GraphBlock>
+				<GraphDraw
+					matrix={[
+						[null, 4, 3],
+						[null, 100, 5],
+						[null, null, null],
+					]}
+				/>
+			</GraphBlock>
+		</Content>
+	);
+};
 
 export default App;
---
+++ src/components/Graph/GraphDraw.jsx
@@ -0,0 +1,71 @@
+import Graph from "vis-react";
+
+const GraphDraw = ({ matrix }) => {
+	let nodes = [];
+	let edges = [];
+	for (let i = 1; i < matrix.length + 1; i++) {
+		nodes.push({ id: i, label: i });
+	}
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			if (matrix[i][j] !== null) {
+				edges.push({
+					from: i + 1,
+					to: j + 1,
+					arrows: "to",
+					label: matrix[i][j],
+				});
+			}
+		}
+	}
+	let graph = {
+		nodes: nodes,
+		edges: edges,
+	};
+	var options = {
+		nodes: {
+			borderWidth: 1,
+			size: 45,
+			color: {
+				border: "#222222",
+				background: "grey",
+			},
+			font: { color: "black", size: 11, face: "arial" },
+		},
+
+		edges: {
+			arrows: {},
+			color: {},
+			font: {
+				color: "#343434",
+				size: 11, // px
+				face: "arial",
+				background: "none",
+				strokeWidth: 5, // px
+				strokeColor: "#ffffff",
+				align: "vertical",
+			},
+			smooth: {
+				enabled: false, //setting to true enables curved lines
+				//type: "dynamic",
+				//roundness: 0.5
+			},
+			physics: { enabled: false },
+		},
+	};
+	// eslint-disable-next-line no-console
+	console.log(edges);
+	return (
+		<Graph
+			graph={graph}
+			options={options}
+			// events={events}
+			// style={style}
+			// getNetwork={this.getNetwork}
+			// getEdges={this.getEdges}
+			// getNodes={this.getNodes}
+			// vis={(vis) => (this.vis = vis)}
+		/>
+	);
+};
+export default GraphDraw;
